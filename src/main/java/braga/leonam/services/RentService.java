c5968d699215cd37782c8889d587d76ae90f616d
LeonamBr <62444284+LeonamBr@users.noreply.github.com>
2022-02-22 16:27:17 -0300

    Criação da classe RentServiceTest e colocado na src/test

--- src/main/java/braga/leonam/services/RentService.java
+++ src/main/java/braga/leonam/services/RentService.java
@@ -2,9 +2,6 @@ package braga.leonam.services;
 
 import java.time.Instant;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import braga.leonam.entities.Movie;
 import braga.leonam.entities.Rent;
 import braga.leonam.entities.User;
@@ -20,21 +17,6 @@ public class RentService {
 		rent.setReturnDate(rent.getRentDate().plusSeconds(86400));
 
 		return rent;
-	}
-
-	@Test
-	public void test() {
-
-		RentService service = new RentService();
-		User user = new User("Leonam Braga");
-		Movie movie = new Movie("Homem aranha", 3, 2.75);
-				
-		Rent rent = service.rentAMovie(user, movie);
-		
-		Assert.assertTrue(rent.getValue() == 2.75);
-		Assert.assertTrue(rent.getRentDate().equals(Instant.now()));
-		Assert.assertTrue(rent.getReturnDate().equals(Instant.now().plusSeconds(86400)));
-
 	}
 
 }
---
+++ src/test/java/braga/leonam/services/RentServiceTest.java
@@ -0,0 +1,30 @@
+package braga.leonam.services;
+
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import braga.leonam.entities.Movie;
+import braga.leonam.entities.Rent;
+import braga.leonam.entities.User;
+import braga.leonam.services.RentService;
+
+public class RentServiceTest {
+	
+	@Test
+	public void test() {
+
+		RentService service = new RentService();
+		User user = new User("Leonam Braga");
+		Movie movie = new Movie("Homem aranha", 3, 2.75);
+				
+		Rent rent = service.rentAMovie(user, movie);
+		
+		Assert.assertTrue(rent.getValue() == 2.75);
+		Assert.assertTrue(rent.getRentDate().equals(Instant.now()));
+		Assert.assertTrue(rent.getReturnDate().equals(Instant.now().plusSeconds(86400)));
+
+	}
+
+}
