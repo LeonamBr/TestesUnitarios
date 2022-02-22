e96253eec0992fac4ebe157d67cdbdef61504597
LeonamBr <62444284+LeonamBr@users.noreply.github.com>
2022-02-22 16:54:58 -0300

    Separação de testes de forma mais rigorosa

--- src/test/java/braga/leonam/services/RentServiceTest.java
+++ src/test/java/braga/leonam/services/RentServiceTest.java
@@ -8,21 +8,43 @@ import org.junit.Test;
 import braga.leonam.entities.Movie;
 import braga.leonam.entities.Rent;
 import braga.leonam.entities.User;
-import braga.leonam.services.RentService;
 
 public class RentServiceTest {
-	
+
 	@Test
-	public void test() {
+	public void testValue() {
 
 		RentService service = new RentService();
 		User user = new User("Leonam Braga");
 		Movie movie = new Movie("Homem aranha", 3, 2.75);
-				
+
 		Rent rent = service.rentAMovie(user, movie);
-		
+
 		Assert.assertEquals(2.75, rent.getValue(), .01);
+
+	}
+
+	@Test
+	public void testRentDate() {
+
+		RentService service = new RentService();
+		User user = new User("Leonam Braga");
+		Movie movie = new Movie("Homem aranha", 3, 2.75);
+
+		Rent rent = service.rentAMovie(user, movie);
+
 		Assert.assertTrue(rent.getRentDate().equals(Instant.now()));
+
+	}
+
+	@Test
+	public void testReturnDate() {
+
+		RentService service = new RentService();
+		User user = new User("Leonam Braga");
+		Movie movie = new Movie("Homem aranha", 3, 2.75);
+
+		Rent rent = service.rentAMovie(user, movie);
 		Assert.assertTrue(rent.getReturnDate().equals(Instant.now().plusSeconds(86400)));
 
 	}
